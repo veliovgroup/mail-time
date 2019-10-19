8de03de99ceb7517b92e6e55418d87e580e64e37
dr.dimitru <ceo@veliov.com>
2019-10-19 04:04:57 +0300

    🚢 v2.3.0 (Meteor)
    - 👷‍♂️ Reduce bundle size
    - 👨‍💻 Overall codebase fixes
    - 👨‍🔬 Test-suite enhancements covering more use cases
    - 📦 `deepmerge` update to `4.1.1` (*was `3.2.1`*)
    - 📦 `josk` update to `2.2.1` (*was `2.1.0`*)
    - 📦 [dev] `bson-ext` update to `2.0.3` (*was `2.0.2`*)
    - 📦 [dev] `mocha ` update to `6.2.2` (*was `6.0.2`*)
    - 📦 [dev] `mongodb ` update to `3.3.3` (*was `3.2.7`*)
    - 📦 [dev] `nodemailer ` update to `6.3.1` (*was `6.1.1`*)
    
    __New__:
    - ✨ `zombieTime` - Time in *milliseconds*, after this period - pending
    email will be interpreted as "*zombie*". This parameter allows to
    rescue pending email from "*zombie* mode" in case when: server was
    rebooted, exception during runtime was thrown, or caused by bad logic
    - ✨ `revolvingInterval` - Interval in *milliseconds* in between queue
    checks
    - ✨ `minRevolvingDelay` - Minimum revolving delay — the minimum delay
    between tasks executions in *milliseconds*
    - ✨ `maxRevolvingDelay` - Maximum revolving delay — the maximum delay
    between tasks executions in *milliseconds*
    
    For more details — read updated documentation

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:mailer',
-  version: '2.2.0',
+  version: '2.3.0',
   summary: 'Bulletproof email queue on top of NodeMailer with support of multiple clusters and servers setup',
   git: 'https://github.com/VeliovGroup/Mail-Time',
   documentation: 'README.md'
@@ -14,11 +14,11 @@ Package.onUse((api) => {
 
 Package.onTest((api) => {
   api.use('jquery', 'client');
-  api.use(['ecmascript', 'accounts-base', 'ostrio:mailer', 'practicalmeteor:mocha', 'practicalmeteor:chai', 'meteortesting:mocha'], 'server');
-  api.addFiles('test-meteor.js', 'server');
+  api.use(['ecmascript', 'accounts-base', 'ostrio:mailer', 'practicalmeteor:chai', 'meteortesting:mocha'], 'server');
+  api.addFiles('test/meteor.js', 'server');
 });
 
 Npm.depends({
-  'josk': '2.1.0',
-  'deepmerge': '3.2.1'
+  'josk': '2.2.0',
+  'deepmerge': '4.1.1'
 });
