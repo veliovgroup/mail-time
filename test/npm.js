c41832b7cfff954e885af17ab03f06fdabed399d
dr.dimitru <ceo@veliov.com>
2022-05-27 06:53:38 +0300

    📦 NPM.js tests-suite

--- test/npm.js
+++ test/npm.js
@@ -5,7 +5,7 @@ if (!process.env.MONGO_URL) {
 const MailTime        = require('../index.js');
 const nodemailer      = require('nodemailer');
 const directTransport = require('nodemailer-direct-transport');
-const { MongoClient, Cursor: MongoCursor } = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 const mongoAddr  = (process.env.MONGO_URL || '');
 const dbName     = mongoAddr.split('/').pop().replace(/\/$/, '');
@@ -13,47 +13,37 @@ const transports = [];
 const DEBUG      = process.env.DEBUG === 'true' ? true : false;
 const domain     = process.env.EMAIL_DOMAIN || 'example.com';
 const TEST_TITLE = 'testSuiteNPM';
-const CHECK_TIMEOUT = 2048;
+const CHECK_TIMEOUT = 1024;
 
-const { assert }       = require('chai');
-const { it, describe } = require('mocha');
+const { assert } = require('chai');
+const { it, describe, before } = require('mocha');
 
-describe('Has MailTime Object', () => {
-  it('MailTime is Constructor', () => {
-    assert.isFunction(MailTime, 'MailTime is Constructor');
-    assert.equal(typeof MailTime.Template === 'string', true, 'mailQueue has Template');
-  });
+let db;
+let client;
+const mailTimes = {};
 
-  it('Change MailTime.Template', () => {
-    assert.equal(MailTime.Template === '{{{html}}}', false, 'mailQueue.Template has original value');
-    MailTime.Template = '{{{html}}}';
-    assert.equal(MailTime.Template === '{{{html}}}', true, 'mailQueue.Template has new value');
-  });
-});
-
-describe('MailTime Instance', async function () {
-  this.slow(4000);
-  this.timeout(30000);
-
-  const client = await MongoClient.connect(mongoAddr, {
-    j: true,
-    w: 'majority',
-    wtimeout: 30000,
-    poolSize: 15,
-    // readConcern: {
-    //   level: 'majority'
-    // },
+before(async function () {
+  client = await MongoClient.connect(mongoAddr, {
+    writeConcern: {
+      j: true,
+      w: 'majority',
+      wtimeout: 30000
+    },
+    readConcern: {
+      level: 'majority'
+    },
     readPreference: 'primary',
+    // poolSize: 15,
     // reconnectTries: 60,
     socketTimeoutMS: 720000,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     connectTimeoutMS: 120000,
     // reconnectInterval: 3072,
-    connectWithNoPrimary: false,
+    // connectWithNoPrimary: false,
     appname: 'mail-time-test-suite'
   });
-  const db = client.db(dbName);
+  db = client.db(dbName);
 
   transports.push(nodemailer.createTransport(directTransport({
     pool: false,
@@ -75,38 +65,61 @@ describe('MailTime Instance', async function () {
     strategy: 'balancer'
   };
 
+  mailTimes.WithoutConcatenation = new MailTime(Object.assign({
+    concatEmails: false,
+    prefix: `${TEST_TITLE}WithoutConcatenation`
+  }, defaultQueueOptions));
+  await mailTimes.WithoutConcatenation.collection.deleteMany({});
+
+  mailTimes.WithConcatenation = new MailTime(Object.assign({
+    concatEmails: true,
+    prefix: `${TEST_TITLE}WithConcatenation`
+  }, defaultQueueOptions));
+  await mailTimes.WithConcatenation.collection.deleteMany({});
+});
+
+describe('Has MailTime Object', function () {
+  it('MailTime is Constructor', () => {
+    assert.isFunction(MailTime, 'MailTime is Constructor');
+    assert.equal(typeof MailTime.Template === 'string', true, 'MailTime# has Template');
+  });
+
+  it('Change MailTime.Template', () => {
+    assert.equal(MailTime.Template === '{{{html}}}', false, 'MailTime#Template has original value');
+    MailTime.Template = '{{{html}}}';
+    assert.equal(MailTime.Template === '{{{html}}}', true, 'MailTime#Template has new value');
+  });
+});
+
+describe('MailTime Instance', function () {
+  this.slow(4000);
+  this.timeout(8000);
+
   const runTests = (type, concat) => {
-    const mailQueue = new MailTime(Object.assign({
-      concatEmails: concat,
-      prefix: `${TEST_TITLE}${type}`
-    }, defaultQueueOptions));
-
-    mailQueue.collection.deleteMany({});
-
     describe(type, () => {
       describe('MailTime Instance', function () {
         it('Check MailTime instance properties', function () {
-          assert.instanceOf(mailQueue, MailTime, 'mailQueue is instance of MailTime');
-          assert.instanceOf(mailQueue.callbacks, Object, 'mailQueue has callbacks');
-          assert.equal(mailQueue.type, 'server', 'mailQueue has type');
-          assert.equal(mailQueue.prefix, `${TEST_TITLE}${type}`, 'mailQueue has prefix');
-          assert.equal(mailQueue.debug, DEBUG, 'mailQueue has debug');
-          assert.equal(mailQueue.maxTries, 60, 'mailQueue has maxTries');
-          assert.equal(mailQueue.interval, 60000, 'mailQueue has interval');
-          assert.equal(mailQueue.template, '{{{html}}}', 'mailQueue has template');
-          assert.equal(mailQueue.zombieTime, 32786, 'mailQueue has zombieTime');
-          assert.equal(mailQueue.strategy, 'balancer', 'mailQueue has strategy');
-          assert.equal(mailQueue.failsToNext, 4, 'mailQueue has failsToNext');
-          assert.instanceOf(mailQueue.transports, Array, 'mailQueue has transports');
-          assert.equal(mailQueue.transport, 0, 'mailQueue has transport');
-          assert.equal(mailQueue.from(), `no-reply@${domain}`, 'mailQueue has from');
-          assert.equal(mailQueue.concatEmails, concat, 'mailQueue has concatEmails');
-          assert.equal(mailQueue.concatSubject, 'Multiple notifications', 'mailQueue has concatSubject');
-          assert.equal(mailQueue.concatDelimiter, '<hr>', 'mailQueue has concatDelimiter');
-          assert.equal(mailQueue.concatThrottling, 60000, 'mailQueue has concatThrottling');
-          assert.equal(mailQueue.revolvingInterval, 1536, 'mailQueue has revolvingInterval');
-          assert.equal(mailQueue.minRevolvingDelay, 512, 'mailQueue has minRevolvingDelay');
-          assert.equal(mailQueue.maxRevolvingDelay, 2048, 'mailQueue has maxRevolvingDelay');
+          assert.instanceOf(mailTimes[type], MailTime, 'mailTimes[type] is instance of MailTime');
+          assert.instanceOf(mailTimes[type].callbacks, Object, 'mailTimes[type] has callbacks');
+          assert.equal(mailTimes[type].type, 'server', 'mailTimes[type] has type');
+          assert.equal(mailTimes[type].prefix, `${TEST_TITLE}${type}`, 'mailTimes[type] has prefix');
+          assert.equal(mailTimes[type].debug, DEBUG, 'mailTimes[type] has debug');
+          assert.equal(mailTimes[type].maxTries, 60, 'mailTimes[type] has maxTries');
+          assert.equal(mailTimes[type].interval, 60000, 'mailTimes[type] has interval');
+          assert.equal(mailTimes[type].template, '{{{html}}}', 'mailTimes[type] has template');
+          assert.equal(mailTimes[type].zombieTime, 32786, 'mailTimes[type] has zombieTime');
+          assert.equal(mailTimes[type].strategy, 'balancer', 'mailTimes[type] has strategy');
+          assert.equal(mailTimes[type].failsToNext, 4, 'mailTimes[type] has failsToNext');
+          assert.instanceOf(mailTimes[type].transports, Array, 'mailTimes[type] has transports');
+          assert.equal(mailTimes[type].transport, 0, 'mailTimes[type] has transport');
+          assert.equal(mailTimes[type].from(), `no-reply@${domain}`, 'mailTimes[type] has from');
+          assert.equal(mailTimes[type].concatEmails, concat, 'mailTimes[type] has concatEmails');
+          assert.equal(mailTimes[type].concatSubject, 'Multiple notifications', 'mailTimes[type] has concatSubject');
+          assert.equal(mailTimes[type].concatDelimiter, '<hr>', 'mailTimes[type] has concatDelimiter');
+          assert.equal(mailTimes[type].concatThrottling, 60000, 'mailTimes[type] has concatThrottling');
+          assert.equal(mailTimes[type].revolvingInterval, 1536, 'mailTimes[type] has revolvingInterval');
+          assert.equal(mailTimes[type].minRevolvingDelay, 512, 'mailTimes[type] has minRevolvingDelay');
+          assert.equal(mailTimes[type].maxRevolvingDelay, 2048, 'mailTimes[type] has maxRevolvingDelay');
         });
       });
 
@@ -115,7 +128,7 @@ describe('MailTime Instance', async function () {
         this.timeout(30000);
 
         it('sendMail Template placeholders render', function (done) {
-          mailQueue.sendMail({
+          mailTimes[type].sendMail({
             to: `mail-time-tests-1@${domain}`,
             subject: 'You\'ve got an email!',
             user: 'John',
@@ -123,17 +136,17 @@ describe('MailTime Instance', async function () {
             text: '{{user}}, {{ baseUrl }}',
             html: '<p>Hi {{user}}, {{{ baseUrl }}}</p>',
             template: '{{{html}}} {{baseUrl}}',
-            sendAt: new Date
+            sendAt: new Date(Date.now() + CHECK_TIMEOUT + 512)
           });
 
           setTimeout(() => {
-            mailQueue.collection.findOne({
+            mailTimes[type].collection.findOne({
               'mailOptions.to': `mail-time-tests-1@${domain}`
             }, (findError, task) => {
               assert.equal(findError, undefined, 'no error');
               assert.isObject(task, 'task is Object');
 
-              const rendered = mailQueue.___compileMailOpts(transports[0], task);
+              const rendered = mailTimes[type].___compileMailOpts(transports[0], task);
               assert.equal(rendered.html, '<p>Hi John, <b>http://example.com</b></p> http://example.com', 'HTML template is properly rendered');
               assert.equal(rendered.text, 'John, http://example.com', 'Text template is properly rendered');
               done();
@@ -142,7 +155,7 @@ describe('MailTime Instance', async function () {
         });
 
         it('sendMail with no template', function (done) {
-          mailQueue.sendMail({
+          mailTimes[type].sendMail({
             to: `mail-time-tests-2@${domain}`,
             subject: 'You\'ve got an email!',
             user: 'John',
@@ -151,13 +164,13 @@ describe('MailTime Instance', async function () {
           });
 
           setTimeout(() => {
-            mailQueue.collection.findOne({
+            mailTimes[type].collection.findOne({
               'mailOptions.to': `mail-time-tests-2@${domain}`
             }, (findError, task) => {
               assert.equal(findError, undefined, 'no error');
               assert.isObject(task, 'task is Object');
 
-              const rendered = mailQueue.___compileMailOpts(transports[0], task);
+              const rendered = mailTimes[type].___compileMailOpts(transports[0], task);
               assert.equal(rendered.html, '<p>Plain text</p>', 'HTML template is properly rendered');
               assert.equal(rendered.text, 'Plain text', 'Text template is properly rendered');
               done();
@@ -166,7 +179,7 @@ describe('MailTime Instance', async function () {
         });
 
         it('sendMail with simple template', function (done) {
-          mailQueue.sendMail({
+          mailTimes[type].sendMail({
             to: `mail-time-tests-3@${domain}`,
             userName: 'Mike',
             subject: 'Sign up confirmation',
@@ -176,13 +189,13 @@ describe('MailTime Instance', async function () {
           });
 
           setTimeout(() => {
-            mailQueue.collection.findOne({
+            mailTimes[type].collection.findOne({
               'mailOptions.to': `mail-time-tests-3@${domain}`
             }, (findError, task) => {
               assert.equal(findError, undefined, 'no error');
               assert.isObject(task, 'task is Object');
 
-              const rendered = mailQueue.___compileMailOpts(transports[0], task);
+              const rendered = mailTimes[type].___compileMailOpts(transports[0], task);
               assert.equal(rendered.html, `<body><div style="text-align: center"><h1>Hello Mike</h1><p><ul><li>Thank you for registration</li><li>Your login: mail-time-tests-3@${domain}</li></ul></p></div></body>`, 'HTML template is properly rendered');
               assert.equal(rendered.text, `Hello Mike, \r\n Thank you for registration \r\n Your login: mail-time-tests-3@${domain}`, 'Text template is properly rendered');
               done();
@@ -191,48 +204,42 @@ describe('MailTime Instance', async function () {
         });
 
         it('sendMail testing concatenation to the same addressee', function (done) {
-          mailQueue.sendMail({
+          mailTimes[type].sendMail({
             to: `mail-time-tests-4@${domain}`,
             userName: 'Concatenator',
-            subject: '{{Concatenator}}: testing concatenation 1',
-            text: '{{Concatenator}}: testing concatenation 1',
-            html: '<b>{{Concatenator}}: testing concatenation 1</b>',
+            subject: '{{userName}}: testing concatenation 1',
+            text: '{{userName}}: testing concatenation 1',
+            html: '<b>{{userName}}: testing concatenation 1</b>',
             template: '<body>{{{html}}}</body>'
           });
 
           setTimeout(() => {
-            mailQueue.sendMail({
+            mailTimes[type].sendMail({
               to: `mail-time-tests-4@${domain}`,
               userName: 'Concatenator',
-              subject: '{{Concatenator}}: testing concatenation 2',
-              text: '{{Concatenator}}: testing concatenation 2',
-              html: '<b>{{Concatenator}}: testing concatenation 2</b>',
+              subject: '{{userName}}: testing concatenation 2',
+              text: '{{userName}}: testing concatenation 2',
+              html: '<b>{{userName}}: testing concatenation 2</b>',
               template: '<body>{{{html}}}</body>'
             });
           }, CHECK_TIMEOUT / 4);
 
-          setTimeout(() => {
-            mailQueue.collection.find({
+          setTimeout(async () => {
+            const qty = await mailTimes[type].collection.countDocuments({
               $or: [{
                 to: `mail-time-tests-4@${domain}`
               }, {
                 'mailOptions.to': `mail-time-tests-4@${domain}`
               }]
-            }, async (findError, taskCursor) => {
-              assert.equal(findError, undefined, 'no error');
-              assert.instanceOf(taskCursor, MongoCursor, 'taskCursor is Cursor');
-
-              const qty = await taskCursor.count();
-
-              if (concat === true) {
-                assert.equal(qty, 1, 'Has single email record with concatenation');
-              } else {
-                assert.equal(qty, 2, 'Has two email records without concatenation');
-              }
-
-              taskCursor.close();
-              done();
             });
+
+            if (concat === true) {
+              assert.equal(qty, 1, 'Has single email record with concatenation');
+            } else {
+              assert.equal(qty, 2, 'Has two email records without concatenation');
+            }
+
+            done();
           }, CHECK_TIMEOUT);
         });
       });
@@ -242,4 +249,3 @@ describe('MailTime Instance', async function () {
   runTests('WithConcatenation', true);
   runTests('WithoutConcatenation', false);
 });
-
