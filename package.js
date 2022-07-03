37b88e6211dde3e3c20a533e8c6e41f2373be961
dr.dimitru <ceo@veliov.com>
2022-07-03 15:21:23 +0300

    📦 v1.3.3
    __Dependencies__:
    - 📦 `josk@3.0.2`, *was `v3.0.1`*
    - 📦 `[dev]` `nodemailer@6.7.6`, *was `v6.7.5`*

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:mailer',
-  version: '2.4.2',
+  version: '2.4.3',
   summary: 'Bulletproof email queue on top of NodeMailer for a single and multi-server setups',
   git: 'https://github.com/veliovgroup/mail-time',
   documentation: 'README.md'
@@ -18,6 +18,9 @@ Package.onTest((api) => {
 });
 
 Npm.depends({
-  josk: '3.0.1',
-  deepmerge: '4.2.2'
+  josk: '3.0.2',
+  deepmerge: '4.2.2',
+  // UNCOMMENT FOR METEOR_TESTS
+  // nodemailer: '6.7.6',
+  // 'nodemailer-direct-transport': '3.0.7'
 });
