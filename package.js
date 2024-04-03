6656d205672b15ab1f7a39fba860439ed7252e43
dr.dimitru <ceo@veliov.com>
2024-04-03 16:47:10 +0300

    📦 Meteor: v2.5.0
    __Changes:__
    
    - 👨‍🔧 Fix minor bug when re-send loop was trying non-existent transport if only single transport defined
    - 👨‍🔧 Fix `.sendMail` callbacks on single-server setup when `{concatEmails: true}` option enabled
    - 👨‍🔧 Fix `.sendMail` callbacks when `{keepHistory: true}` option enabled
    - 📔 Refined documentation
    - 🤝 Compatibility with `mongod` server, tested with `@4.4.29`, `@5.0.25`, `@6.0.14`, and `@7.0.6`
    - 🤝 Compatibility with `mongodb` driver for node.js, tested with `@4.17.2`, `@5.9.2`, and `@6.5.0`
    - 🤝 Compatibility with the latest Meteor.js and its packages
    - 🤝 Compatibility with upcoming `meteor@3.0.0` and its packages
    
    __Dependencies__:
    
    - 📦 `josk@3.1.1`, *was `v3.0.2`*
    - 📦 `deepmerge@4.3.1`, *was `v4.2.2`*
    
    __Dev Dependencies__:
    
    - 📦 `chai@4.4.1`, *was `v4.3.6`*
    - 📦 `nodemailer@6.9.12`, *was `v6.7.8`*
    - 📦 `nodemailer-direct-transport@3.3.2`, *was `v3.0.7`*

--- package.js
+++ package.js
@@ -1,26 +1,30 @@
 Package.describe({
   name: 'ostrio:mailer',
-  version: '2.4.4',
+  version: '2.5.0',
   summary: 'Bulletproof email queue on top of NodeMailer for a single and multi-server setups',
   git: 'https://github.com/veliovgroup/mail-time',
   documentation: 'README.md'
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('1.6');
-  api.use(['mongo', 'ecmascript'], 'server');
+  Npm.depends({
+    josk: '3.1.1',
+    deepmerge: '4.3.1',
+  });
+
+  api.versionsFrom(['1.6', '3.0-beta.0']);
+  api.use(['mongo@1.6.19 || 2.0.0-beta300.0', 'ecmascript@0.16.8 || 0.16.8-beta300.0'], 'server');
   api.mainModule('index.js', 'server');
 });
 
 Package.onTest((api) => {
-  api.use(['ecmascript', 'mongo', 'practicalmeteor:chai', 'meteortesting:mocha'], 'server');
-  api.addFiles('test/meteor.js', 'server');
-});
+  Npm.depends({
+    deepmerge: '4.3.1',
+    nodemailer: '6.9.12',
+    'nodemailer-direct-transport': '3.3.2',
+    chai: '4.4.1',
+  });
 
-Npm.depends({
-  josk: '3.0.2',
-  deepmerge: '4.2.2',
-  // UNCOMMENT FOR METEOR_TESTS
-  // nodemailer: '6.7.8',
-  // 'nodemailer-direct-transport': '3.0.7'
+  api.use(['ecmascript@0.16.8 || 0.16.8-beta300.0', 'mongo@1.6.19 || 2.0.0-beta300.0', 'meteortesting:mocha@2.1.0 || 3.1.0-beta300.0'], 'server');
+  api.addFiles('test/meteor.js', 'server');
 });
