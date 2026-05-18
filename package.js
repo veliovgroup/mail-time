6997c4f340b44c31207b764c86722994cef5e53f
dr.dimitru <ceo@veliov.com>
2026-05-18 17:58:17 +0300

    📦 meteor.js `mailer`

--- package.js
+++ package.js
@@ -1,15 +1,14 @@
 Package.describe({
   name: 'ostrio:mailer',
-  version: '3.0.0',
-  summary: '📮 Email queue extending NodeMailer with multi SMTP transports and horizontally scaled apps support',
+  version: '4.0.0',
+  summary: '📮 Email queue extending NodeMailer with multi-SMTP transports and horizontally scaled apps support',
   git: 'https://github.com/veliovgroup/mail-time',
   documentation: 'README.md'
 });
 
 Package.onUse((api) => {
   Npm.depends({
-    josk: '5.0.0',
-    deepmerge: '4.3.1',
+    josk: '6.0.0',
   });
 
   api.versionsFrom(['1.6', '3.0-beta.0']);
@@ -19,11 +18,9 @@ Package.onUse((api) => {
 
 Package.onTest((api) => {
   Npm.depends({
-    deepmerge: '4.3.1',
-    nodemailer: '6.9.12',
-    'nodemailer-direct-transport': '3.3.2',
-    chai: '4.4.1',
-    redis: '4.6.13',
+    chai: '6.2.2',
+    pg: '8.20.0',
+    redis: '5.12.1',
   });
 
   api.use(['ecmascript@0.16.8 || 0.16.8-beta300.0', 'mongo@1.6.19 || 2.0.0-beta300.0', 'meteortesting:mocha@2.1.0 || 3.1.0-beta300.0'], 'server');
